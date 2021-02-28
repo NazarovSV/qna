7f134a96caff728f8dfe398729844bfb832ee678
NazarovSV <badger.lock@yandex.ru>
2021-02-28 14:27:08 +0300

    Added a test that the question has many answers and added a test solution

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { is_expected.to have_many(:answers) }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 end
