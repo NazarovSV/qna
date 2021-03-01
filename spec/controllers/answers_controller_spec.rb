0892c951f561246f2fc1d52a7c4a1de5b7f21002
NazarovSV <badger.lock@yandex.ru>
2021-03-01 23:47:22 +0300

    add Create method for AnswersController

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -26,4 +26,33 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question answer in the database' do
+        expect do
+          post :create, params: { answer: attributes_for(:answer), question_id: question }
+        end.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to assigns(:answer)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect do
+          post :create,
+               params: { answer: attributes_for(:answer, :invalid), question_id: question }
+        end.to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
