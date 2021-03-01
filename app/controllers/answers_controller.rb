8ff4e4e8c41c41b6de2fbdfaa7716d284c7460ba
NazarovSV <badger.lock@yandex.ru>
2021-03-02 00:04:33 +0300

    add gem Decent exposure

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,13 @@
 class AnswersController < ApplicationController
-  before_action :load_answer, only: :show
-  before_action :find_question, only: %i[create new]
+  expose :answer
+  expose :question
 
   def show; end
 
-  def new
-    @answer = @question.answers.new
-  end
+  def new; end
 
   def create
-    @answer = @question.answers.new(answer_params)
+    @answer = question.answers.new(answer_params)
     if @answer.save
       redirect_to @answer
     else
@@ -21,13 +19,5 @@ class AnswersController < ApplicationController
 
   def answer_params
     params.require(:answer).permit(:body)
-  end
-
-  def load_answer
-    @answer = Answer.find(params[:id])
-  end
-
-  def find_question
-    @question = Question.find(params[:question_id])
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -7,10 +7,6 @@ RSpec.describe AnswersController, type: :controller do
   describe 'GET #show' do
     before { get :show, params: { id: answer } }
 
-    it 'assigns the requested question answer to @answer' do
-      expect(assigns(:answer)).to eq answer
-    end
-
     it 'renders show view' do
       expect(response).to render_template :show
     end
@@ -18,9 +14,6 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'GET #new' do
     before { get :new, params: { question_id: question } }
-    it 'assigns a new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
 
     it 'renders new view' do
       expect(response).to render_template :new
