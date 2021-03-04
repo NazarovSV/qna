c3944b4235c1be7f6bee9fa4a30836952b258c40
NazarovSV <badger.lock@yandex.ru>
2021-03-04 22:45:50 +0300

    User can see the question and its answers

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+feature 'User can see question with answers', %q{
+  To solve the problem,
+  as an authenticated and non-authenticated user,
+  I can look at the question with answers
+} do
+  given(:question) { create(:question) }
+  given!(:answers) { create_list(:answer, 5, question: question) }
+
+  scenario 'Authenticated user sees a question with answers' do
+    user =  create(:user)
+    sign_in(user)
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    answers.each do | answer |
+      expect(page).to have_content answer.body
+    end
+  end
+
+  scenario 'Unauthenticated user sees a question with answers' do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    save_and_open_page
+
+    answers.each do | answer |
+      expect(page).to have_content answer.body
+    end
+  end
+end
