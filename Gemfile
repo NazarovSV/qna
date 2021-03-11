b83fd5b61dae9d26985812bf2665ec290b7ee1a1
NazarovSV <badger.lock@yandex.ru>
2021-03-11 16:44:04 +0300

    Add bootstrap

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'devise'
 gem 'rexml'
 gem 'slim-rails'
 gem 'jquery-rails'
+gem 'bootstrap', '~> 5.0.0.beta1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_message(type)
+    return unless flash[type]
+
+    content_tag :div, class: "alert #{message_type(type)} alert-dismissible fade show", role: 'alert' do
+      content_tag(:span, flash[type]) +
+        button_tag('', type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' }, aria: { label: 'Close' })
+    end
+  end
+
+  def message_type(type)
+    case type
+    when 'notice' then 'alert-success'
+    when 'alert' then 'alert-danger'
+    else
+      'alert-info'
+    end
+  end
 end
