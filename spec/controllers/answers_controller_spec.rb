e4e7d5d8498aab15a1c23053097db36d95c0f0ec
NazarovSV <badger.lock@yandex.ru>
2021-03-12 12:00:58 +0300

    User can edit only his answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -75,30 +75,51 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'PATCH #update' do
-    before { login(create(:user)) }
+    context 'sign in as answers author' do
+      before { login(answer.user) }
 
-    context  'with valid attributes' do
-      it 'changes answer attributes' do
-        patch :update, params: { id: answer, answer: {body: 'New body'} }, format: :js
-        answer.reload
-        expect(answer.body).to eq 'New body'
+      context 'with valid attributes' do
+        it 'changes answer attributes' do
+          patch :update, params: { id: answer, answer: {body: 'New body'} }, format: :js
+          answer.reload
+          expect(answer.body).to eq 'New body'
+        end
+
+        it 'renders update view' do
+          patch :update, params: { id: answer, answer: {body: 'New body'} }, format: :js
+          expect(response).to render_template :update
+        end
       end
 
-      it 'renders update view' do
-        patch :update, params: { id: answer, answer: {body: 'New body'} }, format: :js
-        expect(response).to render_template :update
+      context  'with invalid attributes' do
+        it 'does not change answer attributes' do
+          expect do
+            patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+          end.to_not change(answer, :body)
+        end
+
+        it 'renders update view' do
+          patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+          expect(response).to render_template :update
+        end
+
       end
+
     end
 
-    context  'with invalid attributes' do
+    context "trying to change other people's answers" do
+      before { login(create(:user)) }
+
       it 'does not change answer attributes' do
-        expect do
-          patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
-        end.to_not change(answer, :body)
+        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
+
+        answer.reload
+
+        expect(answer.body).to_not eq 'new body'
       end
 
       it 'renders update view' do
-        patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
         expect(response).to render_template :update
       end
 
