c7a92fa67bab41dc4b21b1c1b4e4fd2eda9f6af8
NazarovSV <badger.lock@yandex.ru>
2021-03-15 12:31:12 +0300

    Adding files when editing a question

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -16,44 +16,59 @@ feature 'User can edit his question', %q{
   end
 
   describe 'Authenticated user', js: true do
-    scenario 'edits his question' do
-      sign_in user
-      visit questions_path
+    describe 'as question author' do
+      background do
+        sign_in user
+        visit questions_path
 
-      click_on 'Edit'
-
-      within '.questions' do
-        fill_in 'Title', with: 'New Title'
-        fill_in 'Body', with: 'New Body'
-        click_on 'Save'
-
-        expect(page).to_not have_content question.title
-        expect(page).to_not have_content question.body
-        expect(page).to have_content 'New Title'
-        expect(page).to_not have_selector 'textarea'
+        click_on 'Edit'
       end
 
-    end
 
-    scenario 'edits his question with errors' do
-      sign_in user
-      visit questions_path
+      scenario 'edits his question' do
 
-      click_on 'Edit'
+        within '.questions' do
+          fill_in 'Title', with: 'New Title'
+          fill_in 'Body', with: 'New Body'
+          click_on 'Save'
 
-      within '.questions' do
-        fill_in 'Title', with: ''
-        fill_in 'Body', with: ''
-        click_on 'Save'
+          expect(page).to_not have_content question.title
+          expect(page).to_not have_content question.body
+          expect(page).to have_content 'New Title'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
 
-        expect(page).to have_content question.title
-        expect(page).to have_content question.body
-        expect(page).to have_selector 'input'
-        expect(page).to have_selector 'textarea'
-        expect(page).to have_content "Title can't be blank"
-        expect(page).to have_content "Body can't be blank"
+      scenario 'add new files for question' do
+        within '.questions' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb","#{Rails.root}/spec/spec_helper.rb"]
+
+          click_on 'Save'
+        end
+
+        visit question_path question
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+
+      scenario 'edits his question with errors' do
+        within '.questions' do
+          fill_in 'Title', with: ''
+          fill_in 'Body', with: ''
+          click_on 'Save'
+
+          expect(page).to have_content question.title
+          expect(page).to have_content question.body
+          expect(page).to have_selector 'input'
+          expect(page).to have_selector 'textarea'
+          expect(page).to have_content "Title can't be blank"
+          expect(page).to have_content "Body can't be blank"
+        end
       end
     end
+
+
     scenario "tries to edit other's question" do
       sign_in create(:user)
       visit questions_path
