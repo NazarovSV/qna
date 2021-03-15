342bc8715dc6ff65a312ae66ae4c88d56273b068
NazarovSV <badger.lock@yandex.ru>
2021-03-15 12:47:51 +0300

    Adding files when editing a answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -17,34 +17,45 @@ feature 'User can edit his answer', %q{
   end
 
   describe 'Authenticated user', js:true do
-    scenario 'edits his answer' do
-      sign_in user
-      visit question_path(question)
+    describe 'as answer author' do
+      background do
+        sign_in user
+        visit question_path(question)
 
-      click_on 'Edit'
-
-      within '.answers' do
-        fill_in 'Your answer', with: 'edited answer'
-        click_on 'Save'
-
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+        click_on 'Edit'
       end
-    end
 
-    scenario 'edits his answer with errors' do
-      sign_in user
-      visit question_path(question)
 
-      click_on 'Edit'
+      scenario 'edits his answer' do
+        within '.answers' do
+          fill_in 'Your answer', with: 'edited answer'
+          click_on 'Save'
 
-      within '.answers' do
-        fill_in 'Your answer', with: ''
-        click_on 'Save'
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
 
-        expect(page).to have_selector 'textarea'
-        expect(page).to have_content "Body can't be blank"
+      scenario 'edits his answer with errors' do
+        within '.answers' do
+          fill_in 'Your answer', with: ''
+          click_on 'Save'
+
+          expect(page).to have_selector 'textarea'
+          expect(page).to have_content "Body can't be blank"
+        end
+      end
+
+      scenario 'add new files for answer' do
+        within '.answers' do
+          attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb","#{Rails.root}/spec/spec_helper.rb"]
+
+          click_on 'Save'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
       end
 
     end
