57225ba79fae8060f38fee48ff76782ac27e5519
NazarovSV <badger.lock@yandex.ru>
2021-05-21 20:34:23 +0300

    GET other users

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,8 +2,11 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   authorize_resource class: User
 
   def me
-    authorize!(:me, current_resource_owner)
     render json: current_resource_owner
+  end
+
+  def other
+    render json: User.where.not(id: current_resource_owner.id)
   end
 
 end
