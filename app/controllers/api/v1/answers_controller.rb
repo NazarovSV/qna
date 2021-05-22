7886089c212d30e02ee82b06b905eb1f05050be3
NazarovSV <badger.lock@yandex.ru>
2021-05-22 21:01:06 +0300

    API for question answers

---
+++ app/controllers/api/v1/answers_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::AnswersController < Api::V1::BaseController
+  authorize_resource
+
+  def index
+    answers = Answer.where(question_id: params[:question_id])
+    render json: answers
+  end
+end
