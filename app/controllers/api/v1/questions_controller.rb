b0ce304852fdd2a4e4ffef55a92b46bc98f84e60
NazarovSV <badger.lock@yandex.ru>
2021-05-23 21:36:24 +0300

    API to edit question

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :find_question, only: %i[show update]
+
   authorize_resource
 
   def index
@@ -7,20 +9,32 @@ class Api::V1::QuestionsController < Api::V1::BaseController
   end
 
   def show
-    render json: Question.find(params[:id])
+    render json: @question
   end
 
   def create
-    @question = current_resource_owner.questions.new(question_params)
+    question = current_resource_owner.questions.new(question_params)
 
-    if @question.save
-      render json: @question, status: :created
+    if question.save
+      render json: question, status: :created
+    else
+      render json: { errors: question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      render json: @question, status: :ok
     else
       render json: { errors: @question.errors }, status: :unprocessable_entity
     end
   end
 
   private
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
   def question_params
     params.require(:question).permit(:title,
