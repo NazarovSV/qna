e1a59d5bcb0f4e4035579c6120f569f2cdf8e0cc
NazarovSV <badger.lock@yandex.ru>
2021-06-07 23:50:49 +0300

    New server

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,13 @@ set :application, "qna"
 set :repo_url, "git@github.com:NazarovSV/qna.git"
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deployer/qna"
-set :deploy_user, 'deployer'
+#
+set :deploy_to, "/home/deploy/qna"
+set :deploy_user, 'deploy'
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", 'storage'
+
